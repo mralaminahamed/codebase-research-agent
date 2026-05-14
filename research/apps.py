a190858ea59a2c2d82a7b87a08aa213fb480c7ee
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2026-05-14 18:33:13 +0600

    feat(research): scaffold research app with stub urls

---
+++ research/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ResearchConfig(AppConfig):
+    """Django app config for the research application."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "research"
